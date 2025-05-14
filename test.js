a8262dcbe8b5979d7c4b82370dff9fe4113e7864
manish <manishmahto198@gmail.com>
2025-05-14 08:57:47 +0530

    added fucntion reverseString

--- test.js
+++ test.js
@@ -1,6 +1,9 @@
-console.log('here i will test git reset command')
+console.log("here i will test git reset command");
 
+function sum(a, b) {
+  return a + b;
+}
 
-function sum(a, b){
-   return a+b
+function reverseString(input) {
+  return input.split("").reverse().join("");
 }
