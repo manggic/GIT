674c274d8c3c57edaaa1736548e5b2bf21171cfa
manish <manishmahto198@gmail.com>
2025-05-14 09:50:39 +0530

    Revert "added a sum function"
    This reverts commit 7630b0a6184944cf0d4dad1dee8068992e380acd.

--- test.js
+++ test.js
@@ -1,8 +1,5 @@
-console.log("here i will test git reset command");
+console.log('here i will test git reset command')
 
-function sum(a, b) {
-  return a + b;
-}
 
 function reverseString(input) {
   return input.split("").reverse().join("");
